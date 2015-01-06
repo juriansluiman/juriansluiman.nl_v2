f723d8c6b14154c1b111ea6df4e837db59612313
Jurian Sluiman <jurian@soflomo.com>
2015-01-06 12:42:35 +0100

    Store prism.js locally and add it to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp   = require('gulp'),
     concat = require('gulp-concat');
-    minify = require('gulp-minify-css');
+    minify = require('gulp-minify-css')
+    uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
     gulp.src(['public/bower_components/normalize.css/normalize.css', 'public/bower_components/skeleton-css/css/skeleton.css', 'public/styles/fonts/fonts.css', 'public/styles/src/juriansluiman.nl.css'])
@@ -11,6 +12,10 @@ gulp.task('default', function() {
     gulp.src('public/styles/src/admin.css')
         .pipe(minify())
         .pipe(gulp.dest('public/styles/dist/'));
+
+    gulp.src('public/scripts/src/prism.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('public/scripts/dist/'));
 
     return;
 });
